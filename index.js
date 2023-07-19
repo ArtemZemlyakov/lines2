789e77bb84c2fd651221c0f4a4194fee61512b3e
ArtemZemlyakov <139268188+ArtemZemlyakov@users.noreply.github.com>
2023-07-19 17:53:50 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let myInfoText = 'Меня зовут Artem. Сейчас я изучаю язык программирования JavaScript на курсе по JavaScript у Maksim. Я хочу стать веб-разработчиком, потому что очень нравится данное направление. До этого я изучал JavaScript 0месяцев. Я уверен, что пройду данный курс до конца!';
+console.log('replaceAll', myInfoText.replaceAll('JavaScript','javascript'),);
+let myInfoText2 = 'Меня зовут Artem. Сейчас я изучаю язык программирования javascript на курсе по javascript у Maksim. Я хочу стать веб-разработчиком, потому что очень нравится данное направление. До этого я изучал javascript 0месяцев. Я уверен, что пройду данный курс до конца!';
+console.log('replaceAll', myInfoText2.replaceAll('курс','КУРС'));
+
+const myInfoText3 = 'Меня зовут Artem. Сейчас я изучаю язык программирования JavaScript на курсе по JavaScript у Maksim. Я хочу стать веб-разработчиком, потому что очень нравится данное направление. До этого я изучал JavaScript 0месяцев. Я уверен, что пройду данный курс до конца!';
+console.log(myInfoText3.length);
+
+const myInfoText4 = 'Меня зовут Artem. Сейчас я изучаю язык программирования JavaScript на курсе по JavaScript у Maksim. Я хочу стать веб-разработчиком, потому что очень нравится данное направление. До этого я изучал JavaScript 0месяцев. Я уверен, что пройду данный курс до конца!';
+console.log(myInfoText4[1]);
+
+const myInfoText5 = 'Меня зовут Artem. Сейчас я изучаю язык программирования JavaScript на курсе по JavaScript у Maksim. Я хочу стать веб-разработчиком, потому что очень нравится данное направление. До этого я изучал JavaScript 0месяцев. Я уверен, что пройду данный курс до конца!';
+console.log(myInfoText5[259 - 1]);
+
+
